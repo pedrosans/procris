2d7b12177668adc0a209dcf87991b95acdddcb9a
Pedro Santos <pedrosans@gmail.com>
2018-02-26 03:21:24 -0300

    Release 1.4

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(name='vimwn',
-	version='1.3',
-	last_version='1.2',
+	version='1.4',
+	last_version='1.3',
 	description='Window navigator that emulates Vim commands',
 	author='Pedro Santos',
 	author_email='pedrosans@gmail.com',
