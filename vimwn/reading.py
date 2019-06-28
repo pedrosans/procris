c6f03627057f5fa8c21e3a371125119839c8bfbe
Pedro Santos <pedrosans@gmail.com>
2019-06-28 00:03:12 -0300

    Add window command mode

--- vimwn/reading.py
+++ vimwn/reading.py
@@ -31,6 +31,7 @@ from vimwn.message import Messages
 class Reading:
 
 	def __init__(self, service=None):
+		self.view = None
 		self.running_as_service = True if service else False
 		self.service = service
 		self.windows = Windows(self)
@@ -57,6 +58,8 @@ class Reading:
 		self.multiplier = ""
 
 	def _initialize_view(self):
+		if self.view:
+			self.view.close()
 		self.view = NavigatorWindow(self, self.windows, self.messages)
 		self.view.connect("key-press-event", self.on_window_key_press)
 		self.view.entry.connect("key-release-event", self.on_entry_key_release)
@@ -67,6 +70,7 @@ class Reading:
 	def start(self, time=0):
 		self.windows.read_screen()
 		# TODO test if read itself at the top
+		# TODO test if the prefix is ctrl-w
 		if self.windows.read_itself:
 			self.windows.cycle(None, None)
 			self.windows.commit_navigation(time)
@@ -78,21 +82,18 @@ class Reading:
 	def end(self, *args):
 		self.set_normal_mode()
 
-	def show_error_message(self, message, time):
-		self._clear_command_ui_state()
-		self.messages.add(message, 'error')
-		self.view.show(time)
-
 	def set_normal_mode(self):
 		self.view.hide()
 		self._clean_state()
 
-	# TODO rename to refresh_ui
-	def refresh_view(self, time):
-		"""
-		Repaints the UI at its default state
-		"""
-		self._clean_state()
+	def set_window_command_mode(self, time, error_message=None):
+		if error_message:
+			self.messages.add(error_message, 'error')
+		self._clear_command_ui_state()
+		self.view.show(time)
+
+	def set_command_mode(self, time):
+		self.reading_command = True
 		self.view.show(time)
 
 	def on_window_key_press(self, widget, event):
@@ -124,8 +125,7 @@ class Reading:
 			return False
 
 		if not self.view.entry.get_text().strip():
-			self._clear_command_ui_state()
-			self.view.show(event.time)
+			self.set_window_command_mode(event.time)
 			return False
 
 		if not self.view.get_command().strip():
@@ -211,7 +211,7 @@ class Reading:
 		if command:
 			command.function(cmd, time)
 		else:
-			self.show_error_message('Not an editor command: ' + cmd, time)
+			self.set_window_command_mode(time, error_message='Not an editor command: ' + cmd)
 
 	#
 	# COMMANDS
@@ -220,9 +220,9 @@ class Reading:
 		self.configurations.reload()
 		self.applications.reload()
 		self.terminal.reload()
-		self.view.close()
 		self._initialize_view()
-		self.refresh_view(time)
+		self.messages.clean()
+		self.set_window_command_mode(time)
 		if self.running_as_service:
 			self.service.reload()
 
@@ -245,11 +245,11 @@ class Reading:
 				self.applications.launch_by_name(app_name)
 				self.set_normal_mode()
 			except GLib.GError as exc:
-				self.show_error_message('Error launching ' + name, time)
+				self.set_window_command_mode(time, error_message='Error launching ' + name)
 		elif len(possible_apps) == 0:
-			self.show_error_message('No matching application for ' + name, time)
+			self.set_window_command_mode(time, error_message='No matching application for ' + name)
 		else:
-			self.show_error_message('More than one application matches: ' + name, time)
+			self.set_window_command_mode(time, error_message='More than one application matches: ' + name)
 
 	def bang(self, cmd, time):
 		vim_cmd = Command.extract_vim_command(cmd)
@@ -258,11 +258,11 @@ class Reading:
 		self.set_normal_mode()
 
 	def colon(self, keyval, time):
-		self.reading_command = True
-		self.view.show (time)
+		self.set_command_mode(time)
 
 	def enter(self, keyval, time):
-		self.refresh_view(time)
+		self.messages.clean()
+		self.set_window_command_mode(time)
 
 	def escape(self, keyval, time):
 		self.set_normal_mode()
@@ -280,7 +280,7 @@ class Reading:
 			self.windows.active.minimize()
 			self.set_normal_mode()
 		else:
-			self.show_error_message('No active window', time)
+			self.set_window_command_mode(time, error_message='No active window')
 
 	def centralize(self, cmd, time):
 		self.windows.centralize_active_window()
@@ -291,9 +291,8 @@ class Reading:
 		self.windows.commit_navigation(time)
 
 	def buffers(self, cmd, time):
-		self._clear_command_ui_state()
 		self.messages.list_buffers()
-		self.view.show(time)
+		self.set_window_command_mode(time)
 
 	def buffer(self, cmd, time):
 		"""
@@ -309,7 +308,7 @@ class Reading:
 		if index < len(self.windows.buffers):
 			self.windows.open(self.windows.buffers[index], time)
 		else:
-			self.show_error_message('Buffer {} does not exist'.format(buffer_number), time)
+			self.set_window_command_mode(time, error_message='Buffer {} does not exist'.format(buffer_number))
 
 	def open_named_buffer(self, cmd, time):
 		window_title = Command.extract_text_parameter(cmd)
@@ -317,7 +316,7 @@ class Reading:
 		if w:
 			self.windows.open(w, time)
 		else:
-			self.show_error_message('No matching buffer for ' + window_title, time)
+			self.set_window_command_mode(time, error_message='No matching buffer for ' + window_title)
 
 	# TODO: remove duplicated tokenizer
 	def delete_current_buffer(self, cmd, time):
@@ -325,7 +324,7 @@ class Reading:
 			self.windows.remove(self.windows.active, time)
 			self.set_normal_mode()
 		else:
-			self.show_error_message('There is no active window')
+			self.set_window_command_mode(time, error_message='There is no active window')
 
 	def delete_indexed_buffer(self, cmd, time):
 		to_delete = []
@@ -334,7 +333,7 @@ class Reading:
 			if index < len(self.windows.buffers):
 				to_delete.append(self.windows.buffers[index])
 			else:
-				self.show_error_message('No buffers were deleted', time)
+				self.set_window_command_mode(time, error_message='No buffers were deleted')
 				return
 		for window in to_delete:
 			self.windows.remove(window, time)
@@ -347,5 +346,5 @@ class Reading:
 			self.windows.remove(w, time)
 			self.set_normal_mode()
 		else:
-			self.show_error_message('No matching buffer for ' + window_title, time)
+			self.set_window_command_mode(time, error_message='No matching buffer for ' + window_title)
 
