ed8220d968864f3d4bb203b3f5fadb07b18e0a84
Pedro Santos <pedrosans@gmail.com>
2019-11-01 22:51:39 -0300

    Add scalable icons

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@
 import poco.layout
 from distutils.core import setup
 
-icons = []
+icons = [
+	('/usr/share/icons/hicolor/symbolic', ['data/icon/poco.svg']),
+	('/usr/share/icons/hicolor/scalable/apps', ['data/icon/poco.svg']),
+]
+
 for size in (16, 48, 256):
 	for name_diff in ('', '-light'):
 		for layout_key in [''] + list(poco.layout.FUNCTIONS_MAP.keys()):
@@ -14,10 +18,8 @@ for size in (16, 48, 256):
 				'/usr/share/icons/hicolor/{}x{}/apps'.format(size, size),
 				['data/icon/{}x{}/poco{}{}.png'.format(size, size, key_func_name_diff, name_diff)]))
 
-# ('/usr/share/icons/hicolor/symbolic', ['data/icon/poco.svg']),
-# ('/usr/share/icons/hicolor/scalable/apps', ['data/icon/poco.svg']),
-
-setup(name='poco',
+setup(
+	name='poco',
 	version='0.7',
 	description='Maps Vim window commands to Libwnck functions to move and navigate around X windows',
 	author='Pedro Santos',
@@ -26,8 +28,9 @@ setup(name='poco',
 	classifiers=['License :: GPL3'],
 	packages=['poco'],
 	scripts=['bin/poco'],
-	data_files=icons + [
+	data_files=
+	[
 		('/usr/share/bash-completion/completions/', ['data/completion/poco']),
 		('/usr/share/applications/', ['data/poco.desktop']),
-	],
+	] + icons,
 )
