524ace4429585c93e2c59970547ead513c976fcc
Pedro Santos <pedrosans@gmail.com>
2020-03-23 19:27:09 -0300

    Fix learning test

--- tests/learning/record.py
+++ tests/learning/record.py
@@ -1,17 +1,13 @@
 import warnings
-
-import Xlib
 import gi, threading, sys
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
 from Xlib import X
 from Xlib.ext import record
 from Xlib.display import Display
 from Xlib.protocol import rq
-
 from procris.keyboard import normalize_state, format_key_event
-
-gi.require_version('Gtk', '3.0')
-gi.require_version('Gdk', '3.0')
-from gi.repository import Gtk, Gdk, GObject, GLib
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 CONTEXT_FILTER = [{
 		'core_requests': (0, 0), 'core_replies': (0, 0),
@@ -28,7 +24,6 @@ class Record:
 	def __init__(self):
 
 		self.record_thread = threading.Thread(target=self._record, name='x keyboard listener thread')
-
 		self.recording_connection = Display()
 		self.recording_connection.set_error_handler(self._record_display_error_handler)
 
@@ -39,18 +34,15 @@ class Record:
 		print("RECORD extension version %d.%d" % (r.major_version, r.minor_version))
 		self.context = self.recording_connection.record_create_context(0, [record.AllClients], CONTEXT_FILTER)
 
-		self.mod_keys_set = set()
-
 	def start(self):
 		self.recording_connection.sync()
 		self.record_thread.start()
 
 	def stop(self):
 		if self.record_thread.is_alive():
-			# self.well_connection.record_disable_context(self.context)
-			# self.well_connection.close()
-			self.recording_connection.record_disable_context(self.context)
-			self.recording_connection.close()
+			conn = Display()
+			conn.record_disable_context(self.context)
+			conn.close()
 			print('display stopped recording')
 			self.record_thread.join()
 		print('recording thread ended')
@@ -66,29 +58,16 @@ class Record:
 	def _record_display_error_handler(self, exception, *args):
 		print('Error at record display: {}'.format(exception), file=sys.stderr)
 
-	def _local_display_error_handler(self, exception, *args):
-		print('Error at local display: {}'.format(exception), file=sys.stderr)
-
-	#
-	# Event handling
-	#
 	def handler(self, reply):
 		data = reply.data
 		while len(data):
 			event, data = rq.EventField(None).parse_binary_value(
 				data, self.recording_connection.display, None, None)
 
-			if event.detail in self.mod_keys_set:
-				continue
-
 			if event.type == X.KeyPress:
-				self.handle_keypress(event)
+				format_key_event(event)
 
-	# http://python-xlib.sourceforge.net/doc/html/python-xlib_13.html
-	def handle_keypress(self, event: Xlib.protocol.event.KeyPress):
-		format_key_event(event)
 
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 listener = Record()
 listener.start()
 print('ENTER TO STOP THE READING')
